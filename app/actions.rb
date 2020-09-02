a49645e19c6d10830f947efd468bc21006b77fb3
csb3 <c.s.bredeson@gmail.com>
2020-09-02 05:34:19 +0000

    added erb

--- app/actions.rb
+++ app/actions.rb
@@ -10,7 +10,7 @@ def humanized_time_ago(time_ago_in_minutes)
 end
 
 get '/' do
-  finstagram_post_shark = {
+  @finstagram_post_shark = {
     username: "sharky_j",
     avatar_url: "http://naserca.com/images/sharky_j.jpg",
     photo_url: "http://naserca.com/images/shark.jpg",
@@ -23,7 +23,7 @@ get '/' do
     }]
   }
 
-  finstagram_post_whale = {
+  @finstagram_post_whale = {
     username: "kirk_whalum",
     avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
     photo_url: "http://naserca.com/images/whale.jpg",
@@ -36,7 +36,7 @@ get '/' do
     }]
   }
 
-  finstagram_post_marlin = {
+  @finstagram_post_marlin = {
     username: "marlin_peppa",
     avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
     photo_url: "http://naserca.com/images/marlin.jpg",
@@ -48,5 +48,7 @@ get '/' do
       text: "lunchtime! ;)"
     }]
   }
-  [finstagram_post_shark, finstagram_post_whale, finstagram_post_marlin].to_s
+  [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin].to_s
+ @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
+ erb(:index)
 end
